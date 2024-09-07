2ae4146144d38d9ee1d0d23d30f4ba2721c1d41e
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-09-07 03:44:23 +0000

    fix: broken target link and clean up files

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,178 +1,157 @@
-//import useArticles from '../hooks/useArticles';
 import { RiHeart2Line } from 'react-icons/ri';
 import { BiComment } from 'react-icons/bi';
 import { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
-//import { FaDev, FaTwitter, FaGlobe, FaGithub } from 'react-icons/fa';
 
 async function fetchArticles(page, tag) {
-  const res = await fetch(`https://dev.to/api/articles?per_page=10&page=${page}&tag=${tag}`); 
+  const res = await fetch(`https://dev.to/api/articles?per_page=10&page=${page}&tag=${tag}`);
   if (!res.ok) {
-      throw new Error('Network response failed')
+    throw new Error('Network response failed')
   }
   return res.json();
 }
 
 function Articles() {
-    
-    //const { status, data, error, isFetching } = useArticles();
+  const [page, setPage] = useState(1);
+  const [tag, setTag] = useState("") // leave open so you get most recent articles first
 
-    //const queryClient = useQueryClient()
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-    const [page, setPage] = useState(1);
-    const [tag, setTag] = useState("") // leave open so you get most recent articles first
-  
-    let randomColor =  Math.floor(Math.random() * 16777215).toString(16);
+  const { status, data, error, isFetching } = useQuery(
+    ['articles', page, tag],
+    () => fetchArticles(page, tag),
+    { keepPreviousData: true, staleTime: 5000, refetchOnWindowFocus: false }
+  )
 
-    const { status, data, error, isFetching } = useQuery(
-      ['articles', page, tag],
-      () => fetchArticles(page, tag),
-      { keepPreviousData: true, staleTime: 5000, refetchOnWindowFocus: false}
-    )
 
-    
-    useEffect(() => {
-      window.scrollTo(0, 0);
-      randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    }, [data]);
-    
-    /*
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  }, [data]);
 
-     // Prefetch the next page!
-    useEffect(() => {
-      if (data?.hasMore) {
-        queryClient.prefetchQuery(['projects', page + 1], () =>
-          fetchArticles(page + 1)
-        )
-      }
-    }, [data, page, queryClient])
-
-    */
-    
-  
-    return (
-      <div className="container">
-        <div>
-          {status === "loading" ? (
-            "Loading..."
-          ) : status === "error" ? (
-            <span>Error: {error.message}</span>
-          ) : (
-            <>
-              <nav className="margin-block-20" aria-label="Main">
-                <button onClick={() => setPage(old => Math.max(old - 1, 1))} disabled={page === 1}>Previous</button>
-                <button onClick={() => setPage(old => old + 1 )} disabled={page === 2800}>Next</button>
-              </nav>
-              <div>
-                {data.map((article) => (
-                  <article key={article.id}>
-                   {article.cover_image && 
-                   <img src={article.cover_image} alt={article.title} />
-                   }
-                   <div className="border-top padding-20">
-                     <div className="flex align-center">
+  return (
+    <div className="container">
+      <div>
+        {status === "loading" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <nav className="margin-block-20" aria-label="Main">
+              <button onClick={() => setPage(old => Math.max(old - 1, 1))} disabled={page === 1}>Previous</button>
+              <button onClick={() => setPage(old => old + 1)} disabled={page === 2800}>Next</button>
+            </nav>
+            <div>
+              {data.map((article) => (
+                <article key={article.id}>
+                  {article.cover_image &&
+                    <img src={article.cover_image} alt={article.title} />
+                  }
+                  <div className="border-top padding-20">
+                    <div className="flex align-center">
                       <div className="margin-right-10">
-                          <img className="br-100" src={article.user.profile_image_90} alt="" />
+                        <img className="br-100" src={article.user.profile_image_90} alt="" />
                       </div>
                       <div>
-                          <div className="modal-relative">
-                              <p className="bolder pointer">{article.user.name}</p>
-                              <p>{article.readable_publish_date}</p>
-                          </div>
-                          <div className="modal">
-                            <div className="accent-bg" style={{ backgroundColor:`#${randomColor}` }}></div>
-                            <div className="flex align-center">
-                              <div className="modal-margin">
-                                <img className="br-100 w-50 margin-right-10" src={article.user.profile_image_90} alt="" />
-                              </div>
-                              <h2>{article.user.name}</h2>
+                        <div className="modal-relative">
+                          <p className="bolder pointer">{article.user.name}</p>
+                          <p>{article.readable_publish_date}</p>
+                        </div>
+                        <div className="modal">
+                          <div className="accent-bg" style={{ backgroundColor: `#${randomColor}` }}></div>
+                          <div className="flex align-center">
+                            <div className="modal-margin">
+                              <img className="br-100 w-50 margin-right-10" src={article.user.profile_image_90} alt="" />
                             </div>
-                            <button className="margin-block-20 pointer">Follow</button>
-                            <div className="modal-padding">
+                            <h2>{article.user.name}</h2>
+                          </div>
+                          <button className="margin-block-20 pointer">Follow</button>
+                          <div className="modal-padding">
                             <div>
                               <h3>Profile</h3>
                               <a href={`https://dev.to/${article.user.username}`} target="_blank" rel="noreferrer" className="flex align-center justify-start">
                                 @{article.user.username}
                               </a>
                             </div>
-                              {article.user.website_url && 
+                            {article.user.website_url &&
                               <div>
                                 <h3>Personal</h3>
                                 <a href={article.user.website_url} target="_blank" rel="noreferrer" className="flex align-center justify-start">
                                   Website
                                 </a>
                               </div>
-                              }
-                              {article.user.twitter_username && 
+                            }
+                            {article.user.twitter_username &&
                               <div>
                                 <h3>Twitter</h3>
                                 <a href={`https://twitter.com/${article.user.twitter_username}`} target="_blank" rel="noreferrer" className="flex align-center justify-start">
                                   @{article.user.twitter_username}
                                 </a>
                               </div>
-                              }
-                              {article.user.github_username &&
-                            <div>
-                              <h3>Github</h3>
-                              <a href={`https://github.com/${article.user.github_username}`} target="_blank" rel="noreferrer" className="flex align-center justify-start">
+                            }
+                            {article.user.github_username &&
+                              <div>
+                                <h3>Github</h3>
+                                <a href={`https://github.com/${article.user.github_username}`} target="_blank" rel="noreferrer" className="flex align-center justify-start">
                                   @{article.user.github_username}
                                 </a>
-                            </div>
-                              }
-                            </div>
-                            
+                              </div>
+                            }
                           </div>
-                      </div>
-                     </div>
-                    <div>
-                        <h1 className="title-margin"><a href={article.url} _target="_blank" rel="noreferrer">{article.title}</a></h1>
-                        <div className="flex align-center wrap">
-                          {article.tag_list.map((tag,index) => {
-                              return (
-                                  <div key={index} className="margin-right-10 pointer tag-bg" onClick={()=> setTag(tag)}>
-                                      {`#${tag}`}
-                                  </div>
-                              )
-                          })}
+
                         </div>
+                      </div>
+                    </div>
+                    <div>
+                      <h1 className="title-margin"><a href={article.url} target="_blank" rel="noreferrer">{article.title}</a></h1>
+                      <div className="flex align-center wrap">
+                        {article.tag_list.map((tag, index) => {
+                          return (
+                            <div key={index} className="margin-right-10 pointer tag-bg" onClick={() => setTag(tag)}>
+                              {`#${tag}`}
+                            </div>
+                          )
+                        })}
+                      </div>
                     </div>
                     <div className="flex align-center justify-between">
-                    <div className="flex">
-                      <div className="flex align-center margin-right-30">
+                      <div className="flex">
+                        <div className="flex align-center margin-right-30">
                           <div className="margin-right-10 pointer">
-                              <RiHeart2Line />
+                            <RiHeart2Line />
                           </div>
                           <div>
-                              {article.positive_reactions_count} reactions
+                            {article.positive_reactions_count} reactions
                           </div>
-                      </div>
-                        <div className="flex align-center">
-                            <div className="margin-right-10 pointer">
-                                <BiComment />
-                            </div>
-                            <div>
-                                {article.comments_count} comments
-                            </div>
                         </div>
-                    </div>
-                    <div>
+                        <div className="flex align-center">
+                          <div className="margin-right-10 pointer">
+                            <BiComment />
+                          </div>
+                          <div>
+                            {article.comments_count} comments
+                          </div>
+                        </div>
+                      </div>
+                      <div>
                         {article.reading_time_minutes} min read
+                      </div>
                     </div>
-                    </div>
-                    </div>
-                  </article>
-                ))}
-              </div>
-              <nav className="margin-block-20">
-                <button onClick={() => setPage(old => Math.max(old - 1, 1))} disabled={page === 1}>Previous</button>
-                <button onClick={() => setPage(old => old + 1 )} disabled={page === 2800}>Next</button>
-              </nav>
-              <div>{isFetching ? "Background Updating..." : " "}</div>
-            </>
-          )}
-        </div>
+                  </div>
+                </article>
+              ))}
+            </div>
+            <nav className="margin-block-20">
+              <button onClick={() => setPage(old => Math.max(old - 1, 1))} disabled={page === 1}>Previous</button>
+              <button onClick={() => setPage(old => old + 1)} disabled={page === 2800}>Next</button>
+            </nav>
+            <div>{isFetching ? "Background Updating..." : " "}</div>
+          </>
+        )}
       </div>
-    );
+    </div>
+  );
 }
 
 export default Articles;
@@ -229,4 +208,3 @@ export default Articles;
 }
 ]
 */
-  
